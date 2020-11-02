c75e5c9d346e508c50f60896bdbf55ad70ac8414
Jasmina Heric <jheric1@etf.unsa.ba>
2020-11-02 16:50:54 +0100

    nedostaje intersect()

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,61 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetak;
+    double kraj;
+    boolean log1;
+    boolean log2;
+    public Interval(){
+        pocetak=0;
+        kraj=0;
+        log1=false;
+        log2=false;
+    }
+    public Interval(double poc, double kr, boolean logi1, boolean logi2) {
+        if(poc>kr) throw new IllegalArgumentException();
+        pocetak=poc;
+        kraj=kr;
+        log1=logi1;
+        log2=logi2;
 
+    }
+
+    public static Interval intersect(Interval i, Interval i2) {
+        return i;
+    }
+
+    public boolean isNull(){
+        if(pocetak==0 && kraj==0 && log1==false && log2==false) return true;
+        return false;
+    }
+    public boolean isIn(double tacka){
+        if(tacka>pocetak && tacka<kraj) return true;
+        if(tacka==pocetak && log1==true) return true;
+        if(tacka==kraj && log2==true) return true;
+        else return false;
+    }
+    /*public Interval intersect(Interval i){
+    }
+    public static Interval intersect(Interval i1, Interval i2){
+
+    }
+*/
+    @Override
+    public String toString() {
+        if(isNull()) return "()";
+        if(this.log1 && !this.log2) return "["+this.pocetak+","+this.kraj+")";
+        else if(this.log2 && !this.log1) return "("+this.pocetak+","+this.kraj+"]";
+        else return "("+this.pocetak+","+this.kraj+")";
+    }
+
+    public boolean equals(Interval obj) {
+
+        if (this.pocetak == pocetak && this.kraj == obj.kraj && this.log1 == obj.log1 && this.log2 == obj.log2) return true;
+        return false;
+    }
+
+    public Interval intersect(Interval interval) {
+        return interval;
+    }
 
 }
--- test/ba/unsa/etf/rpr/tutorijal02/IntervalTest.java
+++ test/ba/unsa/etf/rpr/tutorijal02/IntervalTest.java
@@ -54,13 +54,17 @@ class IntervalTest {
         assertEquals("[2.2,2.5)", i2.toString());
     }
 
-    @org.junit.jupiter.api.Test
+
+
+     @org.junit.jupiter.api.Test
     void intersect1() {
         Interval i = new Interval(1.1, 2.5, true, false);
         Interval i2 = new Interval(2.2, 2.6, true, true);
         Interval i3 = Interval.intersect(i, i2);
         assertEquals("[2.2,2.5)", i3.toString());
     }
+
+
 
     @org.junit.jupiter.api.Test
     void equals() {
@@ -76,7 +80,8 @@ class IntervalTest {
         assertFalse(i.equals(i2));
     }
 
-    @org.junit.jupiter.api.Test
+
+   @org.junit.jupiter.api.Test
     void intersect2() {
         Interval i = new Interval(1.1, 10.5, true, false);
         Interval i2 = new Interval(2.2, 2.6, true, true);
@@ -84,11 +89,15 @@ class IntervalTest {
         assertEquals("[2.2,2.6]", i3.toString());
     }
 
-    @org.junit.jupiter.api.Test
+
+
+   @org.junit.jupiter.api.Test
     void intersect3() {
         Interval i = new Interval(-1.1, 2.5, true, false);
         Interval i2 = new Interval(2.2, 3.6, false, true);
         Interval i3 = Interval.intersect(i, i2);
         assertEquals("(2.2,2.5)", i3.toString());
     }
+
+
 }
